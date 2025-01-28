f8699e9421a3dc647327c6fb4a4ceb574905c4ed
Ryann <ryann.hall2004@gmail.com>
2025-01-27 22:50:07 -0500

    displayed and listed musicians

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [musicians, setMusicians] = useState([]);
+  useEffect(()=> {
+    const fetchMusicians = async()=> {
+      const response = await fetch('/api/musicians');
+      const json = await response.json();
+      setMusicians(json);
+    };
+    fetchMusicians();
+  }, []);
+
+  return (
+      <div>
+        <h1>musicians ({ musicians.length })</h1>
+        <ul>
+          {
+            musicians.map((musician)=>{
+              return (
+                <li key={ musician.id }>
+                  { musician.name }
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+  )
+}
+
+export default App
